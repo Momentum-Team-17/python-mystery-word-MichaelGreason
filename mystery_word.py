71da416af44babf32eccb1c1eebf0c2b2d3709cb
MichaelGreason <mgreason18@gmail.com>
2023-02-16 18:08:13 -0500

    started writing the code for the game and setting up the board as a temlpate that can later be replaced through loops while playing, rather than having the board be built via loops.

--- mystery_word.py
+++ mystery_word.py
@@ -1,10 +1,18 @@
+import random
+
+
 def open_file(file):
     # opening the file in read mode
     with open(file) as my_file:
         # reading the file
         data = my_file.read()
         format_data = data.split()
-    return format_data
+        random_word = random.choice(format_data)
+        rw_list = list(random_word)
+        blanks = [' _ '] * len(random_word)
+        print(random_word)
+        print(rw_list)
+    return blanks
 
 
 print(open_file('words.txt'))
@@ -13,27 +21,23 @@ print(open_file('words.txt'))
 # open('test-word.txt')
 # print(read_file)
 
-# def choose_random_word():
-#     random_word = random.choice(open)
-#     print(random_word)
-
 
 # def play_game():
-
-# open_file('words.txt')
+# random_word = random.choice(format_data)
+# return random_word
 
 
 #     word =
 #     wrong_guesses = []
 #     right_guesses = []
 
-#     game_board = ''
-#     for letter in word:
-#         if letter in right_guesses:
-#             game_board += letter + ' '
-#         else:
-#             game_board += '_ '
-#     print(game_board)
+# game_board = ''
+# for letter in word:
+#     if letter in right_guesses:
+#         game_board += letter + ' '
+#     else:
+#         game_board += '_ '
+# print(game_board)
 
 
 # if __name__ == "__main__":
