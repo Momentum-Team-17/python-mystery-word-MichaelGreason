5fc89790a9c5b04e5584c82e604a14236a3d102e
MichaelGreason <mgreason18@gmail.com>
2023-02-16 17:10:10 -0500

    created function open_file whihc opens, reads, and formats data into list

--- mystery_word.py
+++ mystery_word.py
@@ -1,16 +1,40 @@
-def play_game():
-    word = 'apple'
-    wrong_guesses = ['b', 'd']
-    right_guesses = ['a', 'p']
-
-    game_board = ''
-    for letter in word:
-        if letter in right_guesses:
-            game_board += letter + ' '
-        else:
-            game_board += '_ '
-    print(game_board)
+def open_file(file):
+    # opening the file in read mode
+    with open(file) as my_file:
+        # reading the file
+        data = my_file.read()
+        format_data = data.split()
+    return format_data
 
 
-if __name__ == "__main__":
-    play_game()
+print(open_file('words.txt'))
+
+
+# open('test-word.txt')
+# print(read_file)
+
+# def choose_random_word():
+#     random_word = random.choice(open)
+#     print(random_word)
+
+
+# def play_game():
+
+# open_file('words.txt')
+
+
+#     word =
+#     wrong_guesses = []
+#     right_guesses = []
+
+#     game_board = ''
+#     for letter in word:
+#         if letter in right_guesses:
+#             game_board += letter + ' '
+#         else:
+#             game_board += '_ '
+#     print(game_board)
+
+
+# if __name__ == "__main__":
+#     play_game()
