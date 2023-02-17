3f83583f70ec41ff4b8bda322206680ceafc91c2
MichaelGreason <mgreason18@gmail.com>
2023-02-17 15:52:01 -0500

    created a if loop containing a for loop that either replaces the index of underscores with correct guesses or shows the user guess as a wrong guess

--- mystery_word.py
+++ mystery_word.py
@@ -1,35 +1,35 @@
 import random
 
 
-def open_file(file):
-    # opening the file in read mode
+def open_file_start_game(file):
+    # opening the file in read mode and choosing a random word
     with open(file) as my_file:
         # reading the file
         data = my_file.read()
         format_data = data.split()
         random_word = random.choice(format_data)
-        rw_list = list(random_word)
+        rw_as_list = list(random_word)
         blanks = [' _ '] * len(random_word)
-        print(random_word)
-        print(rw_list)
-    return blanks
+        # print(rw_as_list)
+    return rw_as_list, blanks
 
 
-print(open_file('words.txt'))
+def play_game():
+    word, underscores = open_file_start_game('words.txt')
+    print(word, underscores)
+    answer = []
+    wrong_guesses = []
+    user_guess = input("Guess a letter! ")
+    print('user_guess: ', user_guess)
+    if user_guess in word:
+        for i in range(len(word)):
+            if user_guess == word[i]:
+                underscores[i] = user_guess
+    else:
+        wrong_guesses.append(user_guess)
+    print('Answer: ', underscores)
+    print('wrong guesses: ', wrong_guesses)
 
-
-# open('test-word.txt')
-# print(read_file)
-
-
-# def play_game():
-# random_word = random.choice(format_data)
-# return random_word
-
-
-#     word =
-#     wrong_guesses = []
-#     right_guesses = []
 
 # game_board = ''
 # for letter in word:
@@ -38,7 +38,5 @@ print(open_file('words.txt'))
 #     else:
 #         game_board += '_ '
 # print(game_board)
-
-
-# if __name__ == "__main__":
-#     play_game()
+if __name__ == "__main__":
+    play_game()
