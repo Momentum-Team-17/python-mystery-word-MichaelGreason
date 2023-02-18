91f1b800db838ac6214f5a9a61bf8db9cca143a5
MichaelGreason <mgreason18@gmail.com>
2023-02-18 16:47:57 -0500

    fixed bugs including Already guessed letters aspect and play agagain aspect

--- mystery_word.py
+++ mystery_word.py
@@ -10,17 +10,15 @@ def open_file_start_game(file):
         random_word = random.choice(format_data)
         rw_as_list = list(random_word)
         blanks = [' _ '] * len(random_word)
-        # print(rw_as_list)
     return rw_as_list, blanks
 
 
 def play_game():
     word, underscores = open_file_start_game('words.txt')
-    # print(word, underscores)
     print(' '.join(underscores))
     wrong_guesses = []
     difficulty = input(
-        'What kind of challenge do you prefer? Type hard, Medium or Easy: ')
+        'What kind of challenge do you prefer? Type Hard, Medium or Easy: ')
     if difficulty == 'Hard':
         guesses = 8
     elif difficulty == 'Medium':
@@ -33,6 +31,9 @@ def play_game():
         user_guess = input("Guess a letter! ")
         print('user_guess: ', user_guess)
         if user_guess in wrong_guesses:
+            print('Already guessed that! Try again!')
+            guesses += 0
+        elif user_guess in underscores:
             print('Already guessed that! Try again!')
             guesses += 0
         elif user_guess in word:
@@ -52,19 +53,12 @@ def play_game():
         if guesses == 0:
             print('OUT OF GUESSES! The correct answer was ', ' '.join(word))
             break
-    play_again = input('Do you want to play again Y/N? ')
-    if play_again == 'Y' or 'y':
+    play_again = input('Do you want to play again Yes or No? ')
+    if play_again == 'Yes':
         play_game()
     else:
-        print('Okay BYEEEE')
+        print('Okay BYE!')
 
 
-# game_board = ''
-# for letter in word:
-#     if letter in right_guesses:
-#         game_board += letter + ' '
-#     else:
-#         game_board += '_ '
-# print(game_board)
 if __name__ == "__main__":
     play_game()
